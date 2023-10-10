76f5f22e273bec677f1b7800e7be8ffa5745e493
Victor Matheus <matheuscorreaosu@gmail.com>
2023-10-10 11:23:21 -0300

    calcular distancia e mostrar resultado

--- main.c
+++ main.c
@@ -8,9 +8,17 @@ float calcular_distancia(int x1, int x2, int y1, int y2) {
 
 int main(int argc, char *argv[])
 {
-	// definicao das variaveis
-	int x1,x2,x3,x4;
+	int x1,x2,y1,y2;
 	float distancia;
+
+	printf("Informe as coordenadas para o primeiro ponto: ");
+	scanf("%d %d", &x1, &x2);
+	printf("Informe as coordenadas para o segundo ponto: ");
+	scanf("%d %d", &y1, &y2);
+
+	distancia = calcular_distancia(x1, x2, y1, y2);
+
+	printf("Distancia: %.4f", distancia);
 
 	return EXIT_SUCCESS;
 }
